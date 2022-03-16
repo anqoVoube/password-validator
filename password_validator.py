7594c6355955558d2a6c00ed619ea7d7562ee6da
Bakhriddinov Jamoliddin <91607120+anqoVoube@users.noreply.github.com>
2022-03-16 22:27:18 +0000

    First commit

---
+++ password_validator.py
@@ -0,0 +1,43 @@
+class PasswordValidator():
+    __SPECIAL_SYMBOLS = ['$', '@', '#', '%']
+    final_response = {}
+    
+    def __init__(self, password):
+        self.password = password
+    
+    def minlength(self, length):
+        if len(self.password) < length:
+            self.final_response = ["Length less than required"]
+        return self
+    
+    def maxlength(self, length):
+        if len(self.password) > length:
+            self.final_response = ["Length greater than permited"]
+        return self
+    
+    def has_uppercase(self):
+        if not any(str(char).isupper() for char in self.password):
+            self.final_response = ["Password should have at least one uppercase letter"]
+        return self
+    
+    def has_lowercase(self):
+        if not any(str(char).islower() for char in self.password):
+            self.final_response = ["Password should have at least one lowercase letter"]
+        return self
+    
+    def has_numeral(self):
+        if not any(str(char).isdigit() for char in self.password):
+            self.final_response = ['Password should have at least one numeral']
+        return self
+    
+    def has_specific_symbol(self):
+        if not any(char in self.__SPECIAL_SYMBOLS for char in self.password):
+            self.final_response = ['Password should have at least one of those symbols: $@#%']
+        return self
+    
+    def get_response(self):
+        return self.final_response
+ 
+# a = PasswordValidator("Jujo")
+# print(a.minlength(5).has_lowercase().get_response())
+# -> ["Password should have at least one uppercase letter"]
