a79c02404e230d002818e17f2462deadacf23205
Bakhriddinov Jamoliddin <91607120+anqoVoube@users.noreply.github.com>
2022-03-17 03:41:24 +0500

    Debug dictionary changes

--- password_validator.py
+++ password_validator.py
@@ -1,39 +1,39 @@
-class PasswordValidator():
+class PasswordValidator:
     __SPECIAL_SYMBOLS = ['$', '@', '#', '%']
     final_response = {}
     password: str
     
-    def __init__(self, password: str):
+    def __init__(self, password):
         self.password = password
     
     def minlength(self, length: int):
         if len(self.password) < length:
-            self.final_response = ["Length less than required"]
+            self.final_response['min-length'] = ["Length less than required"]
         return self
     
     def maxlength(self, length: int):
         if len(self.password) > length:
-            self.final_response = ["Length greater than permited"]
+            self.final_response['max-length'] = ["Length greater than permited"]
         return self
     
     def has_uppercase(self):
         if not any(str(char).isupper() for char in self.password):
-            self.final_response = ["Password should have at least one uppercase letter"]
+            self.final_response['uppercase'] = ["Password should have at least one uppercase letter"]
         return self
     
     def has_lowercase(self):
         if not any(str(char).islower() for char in self.password):
-            self.final_response = ["Password should have at least one lowercase letter"]
+            self.final_response['lowercase'] = ["Password should have at least one lowercase letter"]
         return self
     
     def has_numeral(self):
         if not any(str(char).isdigit() for char in self.password):
-            self.final_response = ['Password should have at least one numeral']
+            self.final_response['digit'] = ['Password should have at least one numeral']
         return self
     
     def has_specific_symbol(self):
         if not any(char in self.__SPECIAL_SYMBOLS for char in self.password):
-            self.final_response = ['Password should have at least one of those symbols: $@#%']
+            self.final_response['symbol'] = ['Password should have at least one of those symbols: $@#%']
         return self
     
     def get_response(self) -> dict:
